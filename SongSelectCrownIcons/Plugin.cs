84e0d9746a124076d1cf0672706b70fcbf12c54b
Deathblood <deathbloodjr@gmail.com>
2023-05-29 11:05:21 -0500

    Initial commit

---
+++ SongSelectCrownIcons/Plugin.cs
@@ -0,0 +1,92 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections;
+using UnityEngine;
+using BepInEx.Configuration;
+using SongSelectCrownIcons.Patches;
+
+#if TAIKO_IL2CPP
+using BepInEx.Unity.IL2CPP.Utils;
+using BepInEx.Unity.IL2CPP;
+#endif
+
+namespace SongSelectCrownIcons
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, "Song Select Crown Icons", PluginInfo.PLUGIN_VERSION)]
+#if TAIKO_MONO
+    public class Plugin : BaseUnityPlugin
+#elif TAIKO_IL2CPP
+    public class Plugin : BasePlugin
+#endif
+    {
+        public static Plugin Instance;
+        private Harmony _harmony;
+        public new static ManualLogSource Log;
+
+        public ConfigEntry<bool> ConfigEnabled;
+
+#if TAIKO_MONO
+        private void Awake()
+#elif TAIKO_IL2CPP
+        public override void Load()
+#endif
+        {
+            Instance = this;
+
+#if TAIKO_MONO
+            Log = Logger;
+#elif TAIKO_IL2CPP
+            Log = base.Log;
+#endif
+
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            SetupConfig();
+            SetupHarmony();
+        }
+
+        private void SetupConfig()
+        {
+            // I never really used this
+            // I'd rather just use a folder in BepInEx's folder for storing information
+            var userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+            ConfigEnabled = Config.Bind("General",
+                "Enabled",
+                true,
+                "Enables the mod.");
+
+        }
+
+        private void SetupHarmony()
+        {
+            // Patch methods
+            _harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+            if (ConfigEnabled.Value)
+            {
+                _harmony.PatchAll(typeof(SongSelectCrownIconsPatch));
+                Log.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} is loaded!");
+            }
+            else
+            {
+                Log.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} is disabled.");
+            }
+        }
+
+        // I never used these, but they may come in handy at some point
+        public static MonoBehaviour GetMonoBehaviour() => TaikoSingletonMonoBehaviour<CommonObjects>.Instance;
+
+        public void StartCustomCoroutine(IEnumerator enumerator)
+        {
+#if TAIKO_MONO
+            GetMonoBehaviour().StartCoroutine(enumerator);
+#elif TAIKO_IL2CPP
+            GetMonoBehaviour().StartCoroutine(enumerator);
+#endif
+        }
+
+    }
+}
